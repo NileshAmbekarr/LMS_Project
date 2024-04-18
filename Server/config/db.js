74e046b25255960d75b8e0897ad41aef33ab5cdd
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-04-18 11:21:44 +0530

    Added user Controllers

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', false);
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL || `mongodb://127.0.0.1:27017/lms`)
     .then(function (connection) {
         console.log(`Connected to MongoDB : ${connection.host}`);
     }).catch((err)=> {
---
+++ Server/controllers/user.controllers.js
@@ -0,0 +1,90 @@
+import crypto from 'crypto';
+import fs from 'fs/promises'
+
+import cloudinary from 'cloudinary';
+
+import user from '../models/user.model.js';
+
+const cookieOptions = {
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days,
+    httpOnly : true,
+}
+
+// Total of 8 functions are there for a user "Normal User", 
+
+/**
+ * @REGISTER
+ * @ROUTE @POST {{URL}}/api/v1/user/register
+ * @ACCESS Public
+ */
+export const registerUser = asyncHandler(async(req, res, next) => {
+
+})
+
+/**
+ * @LOGIN
+ * @ROUTE @POST {{URL}}/api/v1/user/login
+ * @ACCESS Public
+ */
+export const loginUser = asyncHandler(async(req, res, next) => {
+
+})
+
+/**
+ * @LOGOUT
+ * @ROUTE @POST {{URL}}/api/v1/user/logout
+ * @ACCESS Public
+ */
+
+export const logoutUser = asyncHandler(async(req, res, next) => {
+
+})
+
+/**
+ * @LOGGED_IN_USER_DETAILS
+ * @ROUTE @GET {{URL}}/api/v1/user/me
+ * @ACCESS Private(Logged in users only)
+ */
+
+export const getLoggedInUserDetails = asyncHandler(async (req, res, next) => {
+
+})
+
+/**
+ * @FORGOT_PASSWORD
+ * @ROUTE @POST {{URL}}/api/v1/user/reset
+ * @ACCESS Public
+ */
+export const forgotPassword = asyncHandler(async(req, res, next)=> {
+
+})
+
+/**
+ * @RESET_PASSWORD
+ * @ROUTE @POST {{URL}}/api/v1/user/reset/:resetToken
+ * @ACCESS Public
+ */
+export const resetPassword = asyncHandler(async(req, res, next) => {
+
+})
+
+/**
+ * @CHANGE_PASSWORD
+ * @ROUTE @POST {{URL}}/api/v1/user/change-password
+ * @ACCESS Private (Logged in users only)
+ */
+export const changePassword = asyncHandler(async(req, res, next) => {
+
+})
+
+/**
+ * @UPDATE_USER
+ * @ROUTE @POST {{URL}}/api/v1/user/update/:id
+ * @ACCESS Private (Logged in user only)
+ */
+
+export const updateUser = asyncHandler(async(req, res, next) => {
+
+})
+
